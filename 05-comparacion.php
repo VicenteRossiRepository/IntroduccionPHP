8fbc0cdb13b6870d698f82800da6ada1af79b553
Vicente <vicente.rossi.contacto@gmail.com>
2025-01-09 12:51:23 -0300

    Edicion de archivo 05-comparacion.php de comparativos en php

--- 05-comparacion.php
+++ 05-comparacion.php
@@ -1,5 +1,39 @@
 <?php include 'includes/header.php';
 
+$num1 = 20;
+$num2= 30;
+$num3 = 30;
+$num4 = '30';
+
+var_dump($num1 > $num2);
+echo '<br>';
+var_dump($num1 < $num2);
+echo '<br>';
+var_dump($num1 >= $num2);
+echo '<br>';
+var_dump($num1 <= $num2);
+echo '<br>';
+var_dump($num3 == $num2);
+echo '<br>';
+var_dump($num2 == $num4);
+echo '<br>';
+var_dump($num2 === $num4);
+echo '<br>';
+
+//Comparacion binaria de valores
+
+//-1 si el valor de la izquierda es menor
+var_dump($num1 <=> $num2);
+echo '<br>';
+
+//0 si los valores son iguales
+var_dump($num3 <=> $num2);
+echo '<br>';
+
+//1 si el valor de la izquierda es mayor
+var_dump($num2 <=> $num1);
+echo '<br>';
+
 
 
 
