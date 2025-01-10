30efec49c96f84381ce6e9a3cd2bc4f243d9316a
Vicente <vicente.rossi.contacto@gmail.com>
2025-01-10 20:18:10 -0300

    Edicion de archivo 12-condicionales.php de metodos if, else if, else y switch case

--- 12-condicionales.php
+++ 12-condicionales.php
@@ -1,7 +1,60 @@
 <?php include 'includes/header.php';
 
+$autenticado = true;
+$admin = true;
 
+if ($autenticado && $admin) {
+    echo 'Usuario autenticado correctamente';
+}else{
+    echo 'Usuario no autenticado';
+};
+echo '<br>';
 
+// If anidados
+$cliente = [
+    'nombre' => 'Vicente',
+    'saldo' => 0,
+    'informacion' => [
+        'tipo' => 'Premium'
+    ]
+];
 
+if(empty($cliente)){
+    echo 'El cliente no tiene información';
+}else{
+    echo 'El cliente tiene información';
+    if($cliente['saldo']>0){
+        echo ' El cliente tiene saldo';
+    }else{
+        echo ' El cliente no tiene saldo';
+    }
+};
+echo '<br>';
+
+// else if
+if($cliente['saldo']>0){
+    echo 'El cliente tiene saldo';
+}elseif($cliente['informacion']['tipo'] == 'Premium'){
+    echo 'El cliente es Premium';
+}else{
+    echo 'El cliente no es Premium';
+};
+echo '<br>';
+
+// Switch
+
+$tecnologia = 'JavaScript';
+
+switch($tecnologia){
+    case 'PHP':
+        echo 'El lenguaje de programación es PHP';
+        break;
+    case 'JavaScript':
+        echo 'El lenguaje de programación es JavaScript';
+        break;
+    default:
+        echo 'El lenguaje de programación es desconocido';
+        break;
+};
 
 include 'includes/footer.php';
