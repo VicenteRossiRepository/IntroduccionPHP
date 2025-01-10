79aea2716df272f9abb610f42e0bcda097e2cc4e
Vicente <vicente.rossi.contacto@gmail.com>
2025-01-09 21:20:40 -0300

    Edicion de archivo 09-arrays-assoc de arreglos asociativos

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,24 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Vicente',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'Premium'
+    ]
+];
 
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
+echo $cliente['nombre'];
+echo '<br>';
+echo $cliente['informacion']['tipo'];
+
+$cliente['codigo'] = 123123123;
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 include 'includes/footer.php';
