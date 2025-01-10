6890e28e4b61cf6c3f509cfeb4fa68e010bd5676
Vicente <vicente.rossi.contacto@gmail.com>
2025-01-09 21:12:20 -0300

    Edicion de archivo 08-arrays.php de metodos de arreglos

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,6 +1,31 @@
 <?php include 'includes/header.php';
 
+$carrito = ['Tablet', 'Television', 'Computadora'];
+
+//Acceder a un elemento de un array
+echo $carrito[1];
+
+//Agregar elemento al array
+$carrito[3] = "Nuevo producto...";
+
+//Añadir elemento nuevo al final
+array_push($carrito, 'Mouse');
+
+//Añadir elemento nuevo al inicio
+array_unshift($carrito, 'Audifonos');
+
+//Util para ver contenidos de array
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
+echo "<br>";
 
 
+$cliente = array('Cliente 1', 'Cliente 2','Cliente 3');
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+echo "<br>";
+echo $cliente[1];
 
 include 'includes/footer.php';
