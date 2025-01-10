9f7545583e5b19a0ea79f4b02b4b3cfb6eefaafc
Vicente <vicente.rossi.contacto@gmail.com>
2025-01-09 21:02:43 -0300

    Edicion de archivo 07-string-methods.php explicando metodos de string

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,15 @@
 <?php include 'includes/header.php';
 
+$num1 = 30;
+$num1++;
 
+echo $num1;
+echo ++$num1;
+echo '<br />';
+$num2 = 30;
+$num2--;
+
+echo $num2;
 
 
 include 'includes/footer.php';
--- 07-string-methods.php
+++ 07-string-methods.php
@@ -1,6 +1,43 @@
 <?php include 'includes/header.php';
 
+$nombreCliente= "Vicente Rossi";
 
 
+//Conocer extencion de un string
+echo strlen($nombreCliente);
+echo "<br>";
+var_dump($nombreCliente);
+echo "<br>";
+
+//Eliminar espacios en blanco al principio y al final
+$texto = trim($nombreCliente);
+echo strlen($texto);
+echo "<br>";
+
+//Convertir texto a mayuscula
+echo strtoupper($nombreCliente);
+echo "<br>";
+//Convertir texto a minusculas
+echo strtolower($nombreCliente);
+echo "<br>";
+
+$mail1 = "correo@correo.com";
+$mail2 = "Correo@correo.com";
+
+var_dump(strtolower($mail1) === strtolower($mail2));
+echo "<br>";
+
+echo str_replace('Vicente', 'V.', $nombreCliente);
+echo "<br>";
+
+// Revisar si un string existe o no
+echo strpos($nombreCliente, 'Vicente');
+echo "<br>";
+
+$tipoCliente = 'Premium';
+echo "El cliente ".$nombreCliente." es ".$tipoCliente;
+echo "<br>";
+
+echo "El cliente {$nombreCliente} es {$tipoCliente}";
 
 include 'includes/footer.php';
