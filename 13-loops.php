496237cba6cf92c357ffd2cd68d99f13bac4804f
Vicente <vicente.rossi.contacto@gmail.com>
2025-01-10 20:33:08 -0300

    Edicion del archivo 13-loops.php de ciclos while, do while y for

--- 13-loops.php
+++ 13-loops.php
@@ -1,6 +1,39 @@
 <?php include 'includes/header.php';
 
+//While
+$i = 0;
 
+while($i < 10) {
+    echo $i. '<br>';
+    $i++;
+};
+
+echo '<br>';
+// Do while
+$i = 0;
+do{
+    echo $i. '<br>';
+    $i++;
+}while($i < 10);
+echo '</br>';
+
+//for loop
+for($i = 1; $i <= 100; $i++) {
+    if($i % 3 == 0 && $i % 5 == 0) {
+        echo $i. ' - FIZZ BUZZ <BR>';
+    } 
+    elseif($i % 3 == 0) {
+        echo $i. ' - FIZZ <BR>';
+    }
+    elseif($i % 5 == 0) {
+        echo $i. ' - BUZZ <BR>';
+    }else{
+        echo $i. '<BR>';
+    }
+    
+};
+
+echo '<br>';
 
 
 include 'includes/footer.php';
