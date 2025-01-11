04c22bba3e9538dfc13e2301637ab67647204dfd
Vicente <vicente.rossi.contacto@gmail.com>
2025-01-10 21:20:22 -0300

    Edicion del archivo 15-funciones.php de asignacion de parametros nombrados en funciones

--- 15-funciones.php
+++ 15-funciones.php
@@ -4,10 +4,11 @@ include 'includes/header.php';
 
 
 function sumar(int $num1 = 0, int $num2 = 0){
+    echo $num1;
     echo $num1+$num2;
 }
 
-sumar(10, 20);
+sumar(num2: 10, num1:20);
 echo "<br>";
 
 
