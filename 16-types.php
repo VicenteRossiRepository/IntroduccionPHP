85379317c60c6f43d00771fcd21622de7a43cb3f
Vicente <vicente.rossi.contacto@gmail.com>
2025-01-10 21:16:33 -0300

    Edicion del archivo 16-types.php de retorno de valores en funciones

--- 16-types.php
+++ 16-types.php
@@ -1,7 +1,17 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types=1);
+include 'includes/header.php';
 
+function usuarioAutenticado(bool $autenticado) : ?string{
+    if($autenticado){
+        return "El usuario esta autenticado";
+    }else{
+        return "No autenticado";
+    };
+}
 
+$usuario = usuarioAutenticado(false);
 
-
+echo $usuario;
 
 include 'includes/footer.php';
