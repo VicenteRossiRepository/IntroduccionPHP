98f94aad06878fd48997243f56e085720c05c893
Vicente <vicente.rossi.contacto@gmail.com>
2025-01-10 21:08:02 -0300

    Edicion del archivo 15-funciones.php de creacion y nuso de funciones personalizadas

--- 15-funciones.php
+++ 15-funciones.php
@@ -1,6 +1,14 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types=1);
+include 'includes/header.php';
 
 
+function sumar(int $num1 = 0, int $num2 = 0){
+    echo $num1+$num2;
+}
+
+sumar(10, 20);
+echo "<br>";
 
 
 
