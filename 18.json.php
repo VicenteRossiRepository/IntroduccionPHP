f5aee951be317d0a8e94d323766561a6344814fe
Vicente <vicente.rossi.contacto@gmail.com>
2025-01-10 21:38:46 -0300

    Edicion del archivo 18.json.php del uso de metodos json_encode() y json_decode()

--- 18.json.php
+++ 18.json.php
@@ -1,8 +1,34 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Televisión de 24"',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor curvo',
+        'precio' => 400,
+        'disponible' => false
+    ]
 
+];
 
+echo "<pre>";
+var_dump($productos);
 
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE); //Convertir arreglo a string
+
+$json_array = json_decode($json); //Convertir string a arreglo
+
+var_dump($json);
+var_dump($json_array);
+echo "</pre>";
 
 
 include 'includes/footer.php';
