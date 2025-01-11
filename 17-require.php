6144a7c697c1b224621bf4d20963ef7ff3f4f12c
Vicente <vicente.rossi.contacto@gmail.com>
2025-01-10 21:30:56 -0300

    Edicion del archivo 17-require.php para explicar las funciones require, include y require_once

--- 17-require.php
+++ 17-require.php
@@ -1,8 +1,10 @@
-<?php include 'includes/header.php';
+<?php require 'includes/header.php';
 
+//Include sirve para insertar codigo de otro archivo PHP sin necesidad de escribirlo de nuevo en la pagina
 
+//Require sirve para traer codigo de otras paginas de forma prioritaria 
 
-
+//Require_once sirve para verificar si la pagina ya a sido llamada y si es el caso, lo ignora
 
 
 include 'includes/footer.php';
